132d168a7cc7835c68da36d5da8c65346b2ef0a6
mirbek10 <m51869956@gmail.com>
2024-10-09 18:45:48 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// const name = 'Mirbek'
+// const q1 = 'it'
+// console.log(`менин атым ${name} менин хобим ${q1}`);
+
+// console.log('hello ')
+//  alert('hello')
+//  prompt('how old are you ') 
+//  console.w
+
+
+ var name=prompt("what is your name")
+ console.log("name "+ name);
+ alert("what is yuor name  "+ name)
+
+ let q1 = prompt("how old are you ")
+ console.log()
+
+
+
+
